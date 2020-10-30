9e66d0f2fe09ad8fb95e8244387e037add0de499
kushtej <007kushtej@gmail.com>
2020-10-30 17:30:36 +0530

    added envronment

--- app.js
+++ app.js
@@ -6,15 +6,11 @@ app.use(express.static(publicDir));
 
 app.get("/",function(req,res)
 {
-    //res.sendFile(path.join(__dirname, 'index.htm'));
     res.sendFile( __dirname + "/public/templates/" + "index.html" );
 });
 
-// app.get('/', function (req, res) {
-//     res.send('Hello World')
-//   })
 
 
-app.listen(process.env.PORT || 8000, function(){
-  console.log("app is listening at port %d in %s mode", this.address().port, app.settings.env);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
